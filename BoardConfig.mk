3784aed2e6ecb4429a39fcaa738bab7d7db124ab
huangzihang <584700853@qq.com>
2015-06-02 11:10:28 +0800

    all

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,21 +3,27 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/huawei/CHM_CL00/BoardConfigVendor.mk
 
+TARGET_SPECIFIC_HEADER_PATH := device/huawei/CHM_CL00/include
+
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+TARGET_BOARD_PLATFORM := msm8939
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 TARGET_BOOTLOADER_BOARD_NAME := CHM_CL00
 
+#Kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/huawei/CHM_CL00/mkbootimg.mk
 BOARD_KERNEL_CMDLINE := androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --dt device/huawei/CHM_CL00/dt.img
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
@@ -28,4 +34,24 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/huawei/CHM_CL00/kernel
 
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP
+TARGET_USES_QCOM_BSP := true
+
+#Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_RECOVERY_SWIPE := true
+TARGET_RECOVERY_FSTAB := device/huawei/CHM_CL00/ramdisk/fstab.qcom
+TARGET_USERIMAGES_USE_EXT4 := true
+RECOVERY_USE_MIGRATED_STORAGE := true
+#RECOVERY_USE_VIRTUAL_KEY := true
+#DEVICE_RESOLUTION := 720x1280
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+#COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+
+
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+#BOARD_USE_CUSTOM_RECOVERY_FONT := \"fontcn28_15x40.h\"
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"fontcn30_17x43.h\"
+
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
